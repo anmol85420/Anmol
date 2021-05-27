6e370d7a092bfc1a92cdd91caea7f5d11f808503
Anmol Singh <46889529+anmol85420@users.noreply.github.com>
2021-05-27 15:55:49 +0530

    Add files via upload

---
+++ add/add/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SumDll;
+
+namespace add
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            SumDll.Class1 obj = new SumDll.Class1();
+            textBox3.Text = (obj.Sum(a, b).ToString());
+
+        }
+    }
+}
